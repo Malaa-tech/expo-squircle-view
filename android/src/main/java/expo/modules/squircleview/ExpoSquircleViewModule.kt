8c30af9925f5686da5a7635c6c0a71e1d746c572
Wadah Esam <wadah-wleed@hotmail.com>
2024-08-24 10:04:07 +0300

    fix: android new arch

--- android/src/main/java/expo/modules/squircleview/ExpoSquircleViewModule.kt
+++ android/src/main/java/expo/modules/squircleview/ExpoSquircleViewModule.kt
@@ -9,28 +9,28 @@ class ExpoSquircleViewModule : Module() {
         Name("ExpoSquircleView")
 
         View(ExpoSquircleView::class) {
-            Prop("cornerSmoothing") { view: ExpoSquircleView, prop: Int ->
-                view.setCornerSmoothing(prop)
+            Prop("squircleBackgroundColor") { view: ExpoSquircleView, prop: Int? ->
+                if (prop != null) {
+                    view.setViewBackgroundColor(prop)
+                }
+            }
+
+            Prop("squircleBorderColor") { view: ExpoSquircleView, prop: Int? ->
+                if (prop != null) {
+                    view.setBorderColor(prop)
+                }
+            }
+
+            Prop("squircleBorderWidth") { view: ExpoSquircleView, prop: Float ->
+                view.setBorderWidth(prop)
             }
 
             Prop("borderRadius") { view: ExpoSquircleView, prop: Float ->
                 view.setBorderRadius(prop)
             }
 
-            Prop("backgroundColor") { view: ExpoSquircleView, prop: Int? ->
-                if (prop != null) {
-                    view.setViewBackgroundColor(prop)
-                }
-            }
-
-            Prop("borderColor") { view: ExpoSquircleView, prop: Int? ->
-                if (prop != null) {
-                    view.setBorderColor(prop)
-                }
-            }
-
-            Prop("borderWidth") { view: ExpoSquircleView, prop: Float ->
-                view.setBorderWidth(prop)
+            Prop("cornerSmoothing") { view: ExpoSquircleView, prop: Int ->
+                view.setCornerSmoothing(prop)
             }
 
             Prop("preserveSmoothing") { view: ExpoSquircleView, prop: Boolean ->
