e0b6a706b0dea0cd0c412b3c12d09ae9a80b6b20
Wadah Esam <wadah-wleed@hotmail.com>
2025-04-11 14:43:33 +0300

    basic support

--- src/ExpoSquircleView.web.tsx
+++ src/ExpoSquircleView.web.tsx
@@ -1,18 +1,55 @@
 import * as React from 'react';
 
 import { SquircleButtonProps, SquircleViewProps } from './ExpoSquircleView.types';
+import { getSvgPath } from 'figma-squircle'
+import { TouchableOpacity, View, ViewProps } from 'react-native';
 
-export function SquircleView(props: SquircleViewProps) {
+export function SquircleView(props: ViewProps & SquircleViewProps) {
+  const svgPath = getSvgPath({
+    width: props.style?.width - props.style?.borderWidth,
+    height: props.style?.height - props.style?.borderWidth,
+    cornerRadius: props.style?.borderRadius,
+    cornerSmoothing: (props?.cornerSmoothing / 100) ?? 1,
+    preserveSmoothing: props.preserveSmoothing
+  })
+
+  const borderPath = getSvgPath({
+    width: props.style?.width - props.style?.borderWidth,
+    height: props.style?.height - props.style?.borderWidth,
+    cornerRadius: props.style?.borderRadius,
+    cornerSmoothing: (props?.cornerSmoothing / 100) ?? 1,
+    preserveSmoothing: props.preserveSmoothing
+  })
+
+  console.log({ props })
+
   return (
-    <div>
-    </div>
+
+    <View {...props} style={{
+      ...props.style,
+      backgroundColor: undefined,
+      borderRadius: undefined,
+      borderColor: undefined,
+      borderWidth: undefined,
+      overflow: 'hidden',
+    }}>
+      <svg style={{ position: 'absolute', width: '100%', height: '100%' }}>
+        <path d={svgPath} style={{ transform: `translateX(${props?.style?.borderWidth / 2}px) translateY(${props?.style?.borderWidth / 2}px)` }} fill={props.style?.backgroundColor} />
+      </svg>
+      {props.children}
+      <svg style={{ position: 'absolute', width: '100%', height: '100%' }}>
+        <path d={borderPath} style={{ transform: `translateX(${props?.style?.borderWidth / 2}px) translateY(${props?.style?.borderWidth / 2}px)` }} fill="transparent" stroke={props.style?.borderColor} strokeWidth={props.style?.borderWidth} />
+      </svg>
+    </View>
+
   );
 }
 
 
 export function SquircleButton(props: SquircleButtonProps) {
   return (
-    <div>
-    </div>
+    <TouchableOpacity {...props}>
+      {props.children}
+    </TouchableOpacity>
   );
 }
