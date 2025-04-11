738956d2661e003736f98eb6a7834770a61cce51
Wadah Esam <wadah-wleed@hotmail.com>
2025-04-11 17:42:21 +0300

    fix the function issue

--- src/ExpoSquircleView.tsx
+++ src/ExpoSquircleView.tsx
@@ -6,13 +6,13 @@ import {
   processColor,
   ViewProps,
   Platform,
+  DimensionValue,
 } from "react-native";
 
 import {
   SquircleButtonProps,
   SquircleViewProps,
 } from "./ExpoSquircleView.types";
-import { calculateSquirclePadding } from ".";
 import { NativeView } from "./NativeWrapper";
 
 
@@ -122,6 +122,43 @@ const useSquircleProps = (
         ...(ignoreBorderWidthFromPadding === true ? undefined : calculatedPadding)
       },
     ],
+  };
+};
+
+export const calculateSquirclePadding = (
+  style?: {
+    padding?: DimensionValue;
+    paddingVertical?: DimensionValue;
+    paddingHorizontal?: DimensionValue;
+    paddingBottom?: DimensionValue;
+    paddingEnd?: DimensionValue;
+    paddingLeft?: DimensionValue;
+    paddingRight?: DimensionValue;
+    paddingStart?: DimensionValue;
+    paddingTop?: DimensionValue;
+    borderWidth?: number;
+  },
+  borderWidth?: number
+) => {
+  const extraPadding = borderWidth || style?.borderWidth || 0;
+
+  const calculatePadding = (_paddingValue: DimensionValue) => {
+    if (typeof _paddingValue === "number") {
+      return _paddingValue + extraPadding;
+    }
+    return _paddingValue;
+  };
+
+  return {
+    padding: style?.padding ? calculatePadding(style.padding) : extraPadding,
+    paddingVertical: style?.paddingVertical ? calculatePadding(style.paddingVertical) : undefined,
+    paddingHorizontal: style?.paddingHorizontal ? calculatePadding(style.paddingHorizontal) : undefined,
+    paddingBottom: style?.paddingBottom ? calculatePadding(style.paddingBottom) : undefined,
+    paddingEnd: style?.paddingEnd ? calculatePadding(style.paddingEnd) : undefined,
+    paddingLeft: style?.paddingLeft ? calculatePadding(style.paddingLeft) : undefined,
+    paddingRight: style?.paddingRight ? calculatePadding(style.paddingRight) : undefined,
+    paddingStart: style?.paddingStart ? calculatePadding(style.paddingStart) : undefined,
+    paddingTop: style?.paddingTop ? calculatePadding(style.paddingTop) : undefined,
   };
 };
 
